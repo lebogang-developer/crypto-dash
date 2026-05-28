f6abe3f35f1cb8a808c352732f82f659c9839549
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2026-05-28 05:52:06 +0200

    update

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,7 +1,7 @@
-import { Link, useParams } from "react-router";
-import { useState, useEffect } from "react";
-import Spinner from "../components/Spinner";
-import CoinChart from "../components/CoinChart";
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router';
+import Spinner from '../components/Spinner';
+import CoinChart from '../components/CoinChart';
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
@@ -14,9 +14,8 @@ const CoinDetailsPage = () => {
     const fetchCoin = async () => {
       try {
         const res = await fetch(`${API_URL}/${id}`);
-        if (!res.ok) throw new Error("Failed to fetch coin data!");
+        if (!res.ok) throw new Error('Failed to fetch data');
         const data = await res.json();
-        console.log(data);
         setCoin(data);
       } catch (err) {
         console.log(err);
@@ -30,27 +29,27 @@ const CoinDetailsPage = () => {
   }, [id]);
 
   return (
-    <div className="coin-details-container">
-      <Link to="/">← Back to Home</Link>
+    <div className='coin-details-container'>
+      <Link to='/'>← Back To Home</Link>
 
-      <h1 className="coin-details-title">
-        {coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : "Coin Details"}
+      <h1 className='coin-details-title'>
+        {coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : 'Coin Details'}
       </h1>
 
       {loading && <Spinner />}
-      {error && <div className="error">❌ {error}</div>}
+      {error && <div className='error'>❌ {error}</div>}
 
-      {!loading && !error && coin && (
+      {!loading && !error && (
         <>
           <img
             src={coin.image.large}
             alt={coin.name}
-            className="coin-details-image"
+            className='coin-details-image'
           />
 
-          <p>{coin.description.en.split(". ")[1] + "."}</p>
+          <p>{coin.description.en.split('. ')[0] + '.'}</p>
 
-          <div className="coin-deatils-info">
+          <div className='coin-details-info'>
             <h3>Rank: #{coin.market_cap_rank}</h3>
             <h3>
               Current Price: $
@@ -59,32 +58,26 @@ const CoinDetailsPage = () => {
             <h4>
               Market Cap: ${coin.market_data.market_cap.usd.toLocaleString()}
             </h4>
-
+            <h4>24h High: ${coin.market_data.high_24h.usd.toLocaleString()}</h4>
+            <h4>24h Low: ${coin.market_data.low_24h.usd.toLocaleString()}</h4>
             <h4>
-              24h high: ${coin.market_data.high_24h.usd.toLocaleString()}%
-            </h4>
-            <h4>24h Low: ${coin.market_data.low_24h.usd.toLocaleString()}%</h4>
-
-            <h4>
-              24h Price Change: ${coin.market_data.price_change_24h.toFixed(2)}{" "}
+              24h Price Change: ${coin.market_data.price_change_24h.toFixed(2)}{' '}
               ({coin.market_data.price_change_percentage_24h.toFixed(2)}%)
             </h4>
-
             <h4>
-              Circulating Supply:{" "}
+              Circulating Supply:{' '}
               {coin.market_data.circulating_supply.toLocaleString()}
             </h4>
             <h4>
-              Total Supply:{" "}
-              {coin.market_data.total_supply?.toLocaleString() || "N/A"}
+              Total Supply:{' '}
+              {coin.market_data.total_supply?.toLocaleString() || 'N/A'}
             </h4>
-
             <h4>
-              All-Time High: ${coin.market_data.ath.usd.toLocaleString()} on{" "}
+              All-Time High: ${coin.market_data.ath.usd.toLocaleString()} on{' '}
               {new Date(coin.market_data.ath_date.usd).toLocaleDateString()}
             </h4>
             <h4>
-              All-Time Low: ${coin.market_data.atl.usd.toLocaleString()} on{" "}
+              All-Time Low: ${coin.market_data.atl.usd.toLocaleString()} on{' '}
               {new Date(coin.market_data.atl_date.usd).toLocaleDateString()}
             </h4>
             <h4>
@@ -92,16 +85,16 @@ const CoinDetailsPage = () => {
             </h4>
           </div>
 
-          <CoinChart coinId={id} />
+          <CoinChart coinId={coin.id} />
 
-          <div className="coin-details-links">
+          <div className='coin-details-links'>
             {coin.links.homepage[0] && (
               <p>
-                🌐{" "}
+                🌐{' '}
                 <a
                   href={coin.links.homepage[0]}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Website
                 </a>
@@ -109,24 +102,24 @@ const CoinDetailsPage = () => {
             )}
             {coin.links.blockchain_site[0] && (
               <p>
-                🧩{" "}
+                🧩{' '}
                 <a
                   href={coin.links.blockchain_site[0]}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Blockchain Explorer
                 </a>
               </p>
             )}
             {coin.categories.length > 0 && (
-              <p>Categories: {coin.categories.join(", ")}</p>
+              <p>Categories: {coin.categories.join(', ')}</p>
             )}
-
-            {!loading && !error && !coin && <p>No Data Found!</p>}
           </div>
         </>
       )}
+
+      {!loading && !error && !coin && <p>No Data Found!</p>}
     </div>
   );
 };
